7e53659366575e95a38cf1501d6dea219a364154
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2018-01-28 01:43:29 +0700

    Update range2.js

--- js/range2.js
+++ js/range2.js
@@ -7,8 +7,10 @@
 function range(a, x){
 	if(typeof a != "object") throw Error("first arguments should an array");
 	if(a.constructor != Array) throw Error("first arguments should an array");
+	if(a.length==0) return NaN;
 	//0, 1/a_length, 2*(1/a_length), 3*(1/a_length), ...
 	//                     0<=x<=1
+	if(x==0) return a[0];
 	var spr = 1/(a.length-1);
 	var i = Math.ceil(x / spr);
 	return a[i-1] + ((a[i]-a[i-1]) * (x - (spr * (i-1)))/spr);
