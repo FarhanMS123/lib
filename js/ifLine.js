dcf2ba28ef2d69c7d91a63963f44a977f7c8cdec
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2018-01-27 22:25:19 +0700

    Create ifLine.js

---
+++ js/ifLine.js
@@ -0,0 +1,18 @@
+/*
+ifLine([statement, value_true], [statement, value_true], [sm, vt], ... , [sm, vt]); //will return one of value_true or false
+ifLine([sm, vt], [sm, vt], [sm, vt], ... , [sm, vt], value_false); //will return one of value_true or value_false
+-----------------------------------------------------------------------------------------------------------------------------
+ifLine([false, 3], [false, 4], [true, 5], [false, 6], [true, 7]); //return 5
+ifLine([false, 3], [false, 4], [false, 5], [false, 6], [false, 7]); //return false
+ifLine([false, 3], [false, 4], [false, 5], [false, 6], "another chance"); //return "another chance"
+*/
+
+function ifLine(){
+	arguments = Array.prototype.slice.call(arguments);
+	var i;
+	for(i=0;i<arguments.length;i++){
+		if((/(null|undefined)/ig).test(arguments[i])) continue;
+		if(arguments[i].constructor == Array) if(arguments[i][0] == true) return arguments[i][1];
+	}
+	return false;
+}
