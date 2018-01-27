8ca940337b3b57af4406bddb7f46327592de19ed
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2018-01-28 01:41:02 +0700

    Create range2.js

---
+++ js/range2.js
@@ -0,0 +1,15 @@
+/*
+  a -> array = [0,20,40,60,80,100];
+  x -> number as coordinates = 0.3
+  return 30
+*/
+
+function range(a, x){
+	if(typeof a != "object") throw Error("first arguments should an array");
+	if(a.constructor != Array) throw Error("first arguments should an array");
+	//0, 1/a_length, 2*(1/a_length), 3*(1/a_length), ...
+	//                     0<=x<=1
+	var spr = 1/(a.length-1);
+	var i = Math.ceil(x / spr);
+	return a[i-1] + ((a[i]-a[i-1]) * (x - (spr * (i-1)))/spr);
+}
